82bb4df61b85e59651aa79fe817e471ca87fee1c
amardeep <amardeepjk@gmail.com>
2024-02-20 17:55:37 +0800

    wip

---
+++ _posts/pair.md
@@ -0,0 +1,45 @@
+---
+title: pair in c++
+published: false
+share-description: ""
+tags: [ leetcode ]
+share-img: /img/rust.png
+readtime: true
+permalink: "/sets-in-cpp"
+share-description: "sets in c++"
+comments: false
+---
+std::set<string> stores elements in sorted order.
+so set is techinically a ordered set.
+
+a set can have only unique elements.
+
+So it means set cannot have duplicate elements.
+
+std::set<string> s;
+
+s.insert("abc");
+
+s.insert("abc1");
+
+s.insert("abc1");
+
+--> inserts elements into a set
+
+auto it = s.find("abc");  --> returns a iterator.
+Used to find element of a set.
+
+Loop over all elements of a set
+
+    for(auto ss:s){
+        std::cout << ss << std::endl;
+    }
+
+Loop over all elements of a set using iterators
+
+    for (auto i = s.begin(); i != s.end(); i++) {
+        std::cout << *i << std::endl;
+    }
+
+
+s.erase("abc");  --> Removes elements from a set
--- blog.cpp
+++ blog.cpp
@@ -4,76 +4,35 @@
 // C++ Program to print all permutations
 // of a string in sorted order.
 #include <bits/stdc++.h>
-
 using namespace std;
-
 class Solution {
 public:
-    void nextPermutation(vector<int> &nums) {
-        if (nums.size() == 1)
-            return;
-        set<pair<int, int>> s;
-        bool found = false;
-        s.insert({nums[nums.size() - 1], nums.size() - 1});
-        for (int i = nums.size() - 2; i >= 0; i--) {
-            int current_no = nums[i];
-            pair<int, int> z = {INT_MAX, INT_MAX};
-            bool flag = false;
-            for (auto it: s) {
-                if (it.first > current_no) {
-                    flag = true;
-                    z = min(z, it);
-                }
-            }
-            if (flag) {
-                found = true;
-                s.erase(z);
-                s.insert({current_no, i});
-                nums[i] = z.first;
-                int j = i + 1;
-                for (auto it: s) {
-                    nums[j] = it.first;
-                    j++;
-                }
+    void nextPermutation(vector<int>& nums) {
+        int ind=-1;
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--) {
+            if(nums[i]<nums[i+1]) {
+                ind=i;
                 break;
             }
-            s.insert({nums[i], i});
         }
-        if (!found) {
-            return sort(nums.begin(), nums.end());
+        if(ind==-1) {
+            reverse(nums.begin(),nums.end());
+            return;
         }
-        //stl provided by c++ next_permutation(nums.begin(),nums.end());
-
+        for(int i=n-1;i>ind;i--) {
+            if(nums[i]>nums[ind]) {
+                swap(nums[i],nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin()+ind+1,nums.end());
     }
 };
 
 int main() {
-//    vector<int> rr = {1, 2, 3};
-//    Solution r{};
-//    r.nextPermutation(rr);
-    std::set<string> s;
-    s.insert("abc");
-    s.insert("abc1");
-    s.insert("abc1");
-    auto it1 = s.find("abc");
-    for (auto i = it1->begin(); i < it1->end(); i++) {
-        std::cout << *i << std::endl;
-    }
-
-    for (auto ss: s) {
-        std::cout << ss << std::endl;
-    }
-
-    for (auto i = s.begin(); i != s.end(); i++) {
-        std::cout << *i << std::endl;
-    }
-
-    s.erase("abc");
-    std::cout << 55 << std::endl;
-    for (auto i = s.begin(); i != s.end(); i++) {
-        std::cout << *i << std::endl;
-    }
+    vector<int> rr = {1, 2, 3};
+    Solution r{};
+    r.nextPermutation(rr);
     return 0;
 }
-
-// This is code is contributed by rathbhupendra
