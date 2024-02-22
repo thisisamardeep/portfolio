5008f38a970d823726f80fd9fe52b056fee84e6c
amardeep <amardeepjk@gmail.com>
2024-02-23 05:28:44 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -16,11 +16,26 @@ public:
 
         if (idx == -1) {
             // edge case
-            reverse(nums.begin(),nums.end());
+            reverse(nums.begin(), nums.end());
             return;
 
         } else {
             // need to handle the normal case
+            int rightidx = -1;
+            //Now we start from right and find rightidx such that number at idx is less than number at rightidx
+            for (int j = nums.size() - 1; j > idx; j--) {
+                if (nums[idx] < nums[j]) {
+                    rightidx = j;
+                    break;
+                }
+            }
+            if (rightidx == -1) {
+                // this will never be the case
+            } else {
+                swap(nums[idx], nums[rightidx]);
+                reverse(nums.begin() + idx + 1, nums.end());
+                return;
+            }
         }
     }
 };
