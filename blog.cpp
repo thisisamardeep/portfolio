a483ef5d9888ff3aeed1a5daa081351558a5d4a2
amardeep <amardeepjk@gmail.com>
2024-02-24 12:59:12 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -1,4 +1,32 @@
+
+
+
+class Base {
+public:
+    virtual void someMethod() {}
+
+protected:
+    int m_protectectInt{0};
+private:
+    int m_privateINT{0};
+};
+
+
+class Derived : public Base {
+public:
+    void someother() {};
+
+    void someMethod() override {
+
+    }
+};
+
 int main() {
+    Derived e;
+    e.someother();
+    e.someMethod();
+    Base r;
+    r.
 
     return 0;
 }
