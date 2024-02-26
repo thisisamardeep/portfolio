50420ffb662a1e2a104151ac1b63f81f08a9fde5
amardeep <amardeepjk@gmail.com>
2024-02-27 07:08:47 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -1,80 +1,39 @@
-class Drug {
+class Solution {
 public:
-    Drug(int property) : m_property{property} {};
+    void nextPermutation(vector<int>& nums) {
+        if(nums.size()==1)
+            return;
+        set<pair<int,int>> s;
+        bool found=false;
+        s.insert({nums[nums.size()-1],nums.size()-1});
+        for(int i=nums.size()-2;i>=0;i--){
+            int current_no=nums[i];
+            pair<int,int> z={INT_MAX,INT_MAX};
+            bool flag=false;
+            for(auto it:s){
+                if(it.first>current_no){
+                    flag=true;
+                    z=min(z,it);
+                }
+            }
+            if(flag){
+                found=true;
+                s.erase(z);
+                s.insert({current_no,i});
+                nums[i]=z.first;
+                int j=i+1;
+                for(auto it:s){
+                    nums[j]=it.first;
+                    j++;
+                }
+                break;
+            }
+            s.insert({nums[i],i});
+        }
+        if(!found){
+            return sort(nums.begin(),nums.end());
+        }
+        //stl provided by c++ next_permutation(nums.begin(),nums.end());
 
-    virtual ~Drug() = default;
-
-    virtual void foo() = 0;
-
-    virtual void baz() = 0;
-
-    int m_property;
+    }
 };
-
-class Fruit {
-public:
-    Fruit(double size, int id, char country)
-            : m_size{size}, m_id{id}, m_country{country} {};
-
-    virtual ~Fruit() = default;
-
-    virtual void foo() = 0;
-
-    virtual void bar() {};
-    double m_size;
-    int m_id;
-    char m_country;
-};
-
-class Apple : public Fruit {
-public:
-    Apple(double size, int id, char country, int color)
-            : Fruit{size, id, country}, m_color{color} {};
-
-    ~Apple() = default;
-
-    void foo() override {}
-
-    void get_color() const {}
-
-    virtual void apple_foo() {}
-
-    int m_color;
-};
-
-class Orange : public Fruit, public Drug {
-public:
-    Orange(double size, int id, char country, double weight)
-            : Fruit{size, id, country}, Drug{4}, m_weight{weight} {};
-
-    ~Orange() = default;
-
-    // Overriding Fruit virtual functions.
-    void foo() override {}
-
-    void bar() override {}
-
-    // Overriding Drug virtual functions.
-    void baz() override {}
-
-    // Orange non-virtual functions.
-    void get_weight() const {}
-
-    // Orange virtual functions.
-    virtual void orange_bar() {}
-
-    double m_weight;
-};
-
-int main() {
-    double const apple_size{1.0};
-    int const apple_id{1};
-    char const apple_country{'c'};
-    int const apple_color{2};
-    double const orange_size{0.8};
-    int const orange_id{2};
-    char const orange_country{'n'};
-    double const orange_weight{0.6};
-    Apple apple{apple_size, apple_id, apple_country, apple_color};
-    Orange orange{orange_size, orange_id, orange_country, orange_weight};
-}
