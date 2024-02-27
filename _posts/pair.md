327252a0284592cd723a163c96a01a4bb36185bb
amardeep <amardeepjk@gmail.com>
2024-02-27 20:12:16 +0800

    wip

--- _posts/pair.md
+++ _posts/pair.md
@@ -9,37 +9,19 @@ permalink: "/sets-in-cpp"
 share-description: "sets in c++"
 comments: false
 ---
-std::set<string> stores elements in sorted order.
-so set is techinically a ordered set.
-
-a set can have only unique elements.
-
-So it means set cannot have duplicate elements.
-
-std::set<string> s;
-
-s.insert("abc");
-
-s.insert("abc1");
-
-s.insert("abc1");
-
---> inserts elements into a set
-
-auto it = s.find("abc");  --> returns a iterator.
-Used to find element of a set.
-
-Loop over all elements of a set
-
-    for(auto ss:s){
-        std::cout << ss << std::endl;
-    }
-
-Loop over all elements of a set using iterators
-
-    for (auto i = s.begin(); i != s.end(); i++) {
-        std::cout << *i << std::endl;
-    }
 
 
-s.erase("abc");  --> Removes elements from a set
+How to create a piar.
+There are 2 ways 
+First
+std::pair<int, int> pp(10, 20);
+
+Second Way 
+std::pair<int, int> pp11 = std::make_pair(34, 45);
+
+How to access elements of a pair
+
+    int f = pp.first;
+    int f2 = pp.second;
+
+
--- blog.cpp
+++ blog.cpp
@@ -1,39 +1,61 @@
+#include "set"
+#include "iostream"
+#include "vector"
+#include "climits"
+#include "algorithm"
+
+using namespace std;
+
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        if(nums.size()==1)
+    void nextPermutation(vector<int> &nums) {
+        if (nums.size() == 1)
             return;
-        set<pair<int,int>> s;
-        bool found=false;
-        s.insert({nums[nums.size()-1],nums.size()-1});
-        for(int i=nums.size()-2;i>=0;i--){
-            int current_no=nums[i];
-            pair<int,int> z={INT_MAX,INT_MAX};
-            bool flag=false;
-            for(auto it:s){
-                if(it.first>current_no){
-                    flag=true;
-                    z=min(z,it);
+        set<pair<int, int>> s;
+        bool found = false;
+        s.insert({nums[nums.size() - 1], nums.size() - 1});
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            int current_no = nums[i];
+            pair<int, int> z = {INT_MAX, INT_MAX};
+            bool flag = false;
+            for (auto it: s) {
+                if (it.first > current_no) {
+                    flag = true;
+                    z = min(z, it);
                 }
             }
-            if(flag){
-                found=true;
+            if (flag) {
+                found = true;
                 s.erase(z);
-                s.insert({current_no,i});
-                nums[i]=z.first;
-                int j=i+1;
-                for(auto it:s){
-                    nums[j]=it.first;
+                s.insert({current_no, i});
+                nums[i] = z.first;
+                int j = i + 1;
+                for (auto it: s) {
+                    nums[j] = it.first;
                     j++;
                 }
                 break;
             }
-            s.insert({nums[i],i});
+            s.insert({nums[i], i});
         }
-        if(!found){
-            return sort(nums.begin(),nums.end());
+        if (!found) {
+            return sort(nums.begin(), nums.end());
         }
         //stl provided by c++ next_permutation(nums.begin(),nums.end());
 
     }
 };
+
+
+int main() {
+    std::pair<int, int> pp(10, 20);
+    pp.swap(10);
+    std::pair<int, int> pp11 = std::make_pair(34, 45);
+
+    int f = pp.first;
+    int f2 = pp.second;
+//    Solution e = Solution();
+//    vector<int> e1{3, 4, 5};
+//    e.nextPermutation(e1);
+    return 0;
+}
