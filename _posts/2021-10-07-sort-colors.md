e9cfcdeeadba255cd4ad7e9a29a88b46f3af7115
amardeep <amardeepjk@gmail.com>
2024-03-12 07:35:53 +0800

    wip

---
+++ _posts/2021-10-07-sort-colors.md
@@ -0,0 +1,20 @@
+---
+title: Sort Colors
+published: true
+tags: [ leetcode ]
+readtime: true
+permalink: "/sort-colors"
+share-description: "Sort Colors"
+comments: false
+---
+
+We will solve all the below 3 problems using the similar  logic .
+
+[Sort Colors](https://leetcode.com/problems/sort-colors/description/) 
+
+[Sort List](https://leetcode.com/problems/sort-list/description/) 
+
+[Wiggle Sort](https://leetcode.com/problems/wiggle-sort-ii/description/) 
+
+
+
--- blog.cpp
+++ blog.cpp
@@ -7,21 +7,69 @@
 using namespace std;
 
 
-class Foo {
-    int test;
-    float test_float;
+class Solution {
+public:
+    void
+    amarec(vector<int> &nums, size_t index_first, size_t index_second, size_t index_third, vector<vector<int>> &result
+    ) {
 
-    explicit operator float() const {
-        return test;
+        if (index_third > nums.size() - 1) {
+            return;
+        }
+        if (index_second > nums.size() - 2) {
+            return;
+        }
+        if (index_first > nums.size() - 3) {
+            return;
+        }
+
+        if (index_third == nums.size() - 1) {
+            if (index_second == nums.size() - 2) {
+                amarec(nums, index_first + 1, index_first + 2, index_first + 3, result);
+            } else {
+                amarec(nums, index_first, index_second + 1, index_second + 2, result);
+            }
+
+        } else if (index_third < nums.size() - 1) {
+            amarec(nums, index_first, index_second, index_third + 1, result);
+        }
+
+
+        if (0 == nums[index_first] + nums[index_second] + nums[index_third]) {
+            vector<int> temp{nums[index_first], nums[index_second], nums[index_third]};
+            sort(temp.begin(), temp.end());
+            bool found = false;
+            for (int i = 0; i < result.size(); i++) {
+                if (result[i] == temp) {
+                    found = true;
+                    break;
+                }
+
+            }
+            if (!found) {
+                result.push_back(temp);
+            }
+
+
+        }
+
+
     }
-//    explicit operator float () const {
-//        return test_float;
-//    }
+
+    vector<vector<int>> threeSum(vector<int> &nums) {
+        vector<vector<int>> result{};
+        amarec(nums, 0, 1, 2, result);
+        return result;
+    };
 };
 
+
 int main() {
-    float f;
-    auto i = static_cast<int>(f);
-    std::cout << f << std::endl;
+    Solution df{};
+    vector<int> ee{-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> resul1t{};
+    resul1t = df.threeSum(ee);
+
+
     return 0;
 }
