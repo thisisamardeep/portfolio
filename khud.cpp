3bbe2f420ce0a5f4212afb59caf77feb29b8ad1c
amardeep <amardeepjk@gmail.com>
2024-03-26 06:44:50 +0800

    wip

--- khud.cpp
+++ khud.cpp
@@ -1,8 +1,6 @@
 
 
-//https://leetcode.com/pr131oblems/median-of-two-sorted-arrays/description/?envType=featured-list&envId=top-100-liked-questions?envType=featured-list&envId=top-100-liked-questions
-
-
+//https://leetcode.com/problems/jump-game/description/?envType=study-plan-v2&envId=top-interview-150
 
 
 
@@ -20,30 +18,48 @@ public:
         std::stack<int> t1{};
         int counter_1{0};
         int counter_2{0};
+        int counter_full{0};
+        int break_point = (nums1.size() + nums2.size()) / 2;
 
         while (counter_1 < nums1.size() || counter_2 < nums2.size()) {
             if (counter_1 < nums1.size() && counter_2 < nums2.size()) {
                 if (nums1[counter_1] <= nums2[counter_2]) {
                     t1.push(nums1[counter_1]);
                     counter_1++;
+                    counter_full++;
                 } else {
                     t1.push(nums2[counter_2]);
                     counter_2++;
-
+                    counter_full++;
                 }
 
             } else if (counter_1 < nums1.size() && counter_2 >= nums2.size()) {
                 t1.push(nums1[counter_1]);
                 counter_1++;
+                counter_full++;
+
             } else if (counter_1 >= nums1.size() && counter_2 < nums2.size()) {
                 t1.push(nums2[counter_2]);
                 counter_2++;
+                counter_full++;
+
             }
-
-
+            if (counter_full - 1 == break_point) {
+                break;
+            }
         }
-        int ty = 67;
-        return 0;
+
+        if ((nums2.size() + nums1.size()) % 2 == 0) {
+            double t41 = t1.top();
+            t1.pop();
+            double t42 = t1.top();
+            return (t41 + t42) / 2;
+
+        } else {
+            double t4 = t1.top();
+            return t4;
+        }
+//        return 0;
 
 
     };
@@ -53,8 +69,8 @@ public:
 int main() {
 
 
-    vector<int> nums1{1, 3, 5};
-    vector<int> nums2{2};
+    vector<int> nums1{1, 2};
+    vector<int> nums2{3, 4};
 
 
     Solution s{};
