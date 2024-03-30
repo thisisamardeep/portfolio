58cc00c6c0f7a1ff283e927b66d768fca0fc37cd
amardeep <amardeepjk@gmail.com>
2024-03-30 23:27:56 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -6,8 +6,22 @@
 
 using namespace std;
 
+
+vector<int> makeBigVector() {
+    vector<int> result(1024);
+    for (int i = 0; i < 1024; i++) {
+        result[i] = rand();
+
+    }
+    std::cout << std::addressof(result) << std::endl;
+    return result;
+};
+
+
 int main() {
 
+    vector<int> v = makeBigVector();
+    std::cout << std::addressof(v) << std::endl;
 
     return 0;
 }
