b515b58eccc8d99dfd94f6c609b098aa372d93bb
amardeep <amardeepjk@gmail.com>
2024-04-06 20:22:26 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -3,25 +3,37 @@
 #include "vector"
 #include "climits"
 #include "algorithm"
+#include "cassert"
 
 using namespace std;
 
-
-vector<int> makeBigVector() {
-    vector<int> result(1024);
-    for (int i = 0; i < 1024; i++) {
-        result[i] = rand();
-
-    }
-    std::cout << std::addressof(result) << std::endl;
-    return result;
-};
-
+decltype(auto) f() {
+    static int i = 0;
+    std::cout << i << std::endl;
+    return (i);
+}
 
 int main() {
+//    int tt = 56;
+//    float tt211 = 56.45;
+//
+//    decltype(auto) eee = rr();
+//    f()++;
+//    f()++;
+//    f();
+    int ty = 67;
+//    rrr = 444;
+//    std::cout << rrr << std::endl;
 
-    vector<int> v = makeBigVector();
-    std::cout << std::addressof(v) << std::endl;
+//    std::cout << std::is_same<decltype(tt), decltype(tt211)>::value << std::endl;
+//    std::cout << std::is_same_v<decltype(tt), decltype(tt211)> << std::endl;
+    std::cout << std::is_same_v<decltype((ty)), int &> << std::endl;
+//    assert(std::is_same_v<decltype((ty)), int &>);
+    assert((std::is_same_v<decltype(ty), int>));      // OK: one argument
+    assert((std::is_same_v<decltype((ty)), int &>));      // OK: one argument
+
+    std::cout << std::is_same_v<decltype(ty), int> << std::endl;
+//    static_assert(std::is_same_v<decltype(ty), int &>);
 
     return 0;
 }
