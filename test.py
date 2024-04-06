7b2040e846e8c8cc9400328a93dec309649aadbc
amardeep <amardeepjk@gmail.com>
2024-04-07 06:56:10 +0800

    wip

--- test.py
+++ test.py
@@ -1,76 +1,39 @@
-from recviz import recviz
+from typing import List
 
 
-@recviz
-def fib(n):
-    # base condition mimicking the first two numbers
-    # in the sequence
-    if n == 0: return 0
-    if n == 1: return 1
-
-    # every number is summation of the previous two
-    return fib(n - 1) + fib(n - 2)
-
-
-# fib(3)
-sd = [1, 5, 11, 2]
-
-
-def mergme(inp, start, middle, end):
-    res = []
-    first = inp[start: middle + 1: 1]
-    second = inp[middle + 1:end + 1: 1]
-    print(first)
-    print(second)
-    print(33333333333333)
-
-    i = 0
-    j = 0
-
-    while i < len(first) or j < len(second):
-        print(i)
-        print(j)
-        if i < len(first) and j < len(second):
-            if first[i] <= second[j]:
-                res.append(first[i])
-                i = i + 1
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+        i = 0
+        while i < len(nums):
+            canPass = False
+            if nums[i] == 0:
+                if i == len(nums) - 1:
+                    canPass = True
+                else:
+                    j = 0
+                    while j < i:
+                        if i - j < nums[j]:
+                            canPass = True
+                            break
+                        j = j + 1
+                if canPass:
+                    i = i + 1
+                else:
+                    break
             else:
-                res.append(second[j])
-                j = j + 1
-        elif i < len(first) and j >= len(second):
-            res.append(first[i])
-            i = i + 1
-        elif i >= len(first) and j < len(second):
-            res.append(second[j])
-            j = j + 1
-    return res
+                i = i + 1
+
+        if i == len(nums):
+            return True
+        else:
+            return False
 
 
-def amar(inp, start, end) -> None:
-    if start == end:
-        return
-    # if start == 0 and end == 1:
-    #     return
-    middle = (start + end) // 2
-    amar(inp, start, middle)
-    amar(inp, middle + 1, end)
-    # print("========================")
-    # print(start)
-    # print(end)
-    # print("========================")
-    er = mergme(inp, start, middle, end)
-    inp[start:end + 1:1] = er
-    print(er)
-    ty = 67
-    # inp[start, end + 1, 1] =
-    # print(inp[start:end:1])
-
-
-def driver(inp):
-    # mergeSort(inp, 0, len(inp) - 1)
-    amar(inp, 0, len(inp) - 1)
-    print(inp)
-
-
-# driver(sd)
-driver(sd)
+sd = Solution()
+# nums = [3, 2, 1, 0, 4]
+# nums = [2, 3, 1, 1, 4]
+nums = [2, 0]
+res = sd.canJump(nums=nums)
+print(res)
