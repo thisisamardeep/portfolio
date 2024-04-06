80ab89e973181255c4a50c664bd807458b0ebd88
amardeep <amardeepjk@gmail.com>
2024-04-06 20:38:46 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -3,37 +3,31 @@
 #include "vector"
 #include "climits"
 #include "algorithm"
+
+
+//using namespace std;
+//
+//decltype(auto) f() {
+//    static int i = 0;
+//    std::cout << i << std::endl;
+//    return (i);
+//}
+
+class Foo {
+
+};
+
 #include "cassert"
 
-using namespace std;
-
-decltype(auto) f() {
-    static int i = 0;
-    std::cout << i << std::endl;
-    return (i);
-}
-
 int main() {
-//    int tt = 56;
-//    float tt211 = 56.45;
-//
-//    decltype(auto) eee = rr();
-//    f()++;
-//    f()++;
-//    f();
-    int ty = 67;
-//    rrr = 444;
-//    std::cout << rrr << std::endl;
 
-//    std::cout << std::is_same<decltype(tt), decltype(tt211)>::value << std::endl;
-//    std::cout << std::is_same_v<decltype(tt), decltype(tt211)> << std::endl;
-    std::cout << std::is_same_v<decltype((ty)), int &> << std::endl;
-//    assert(std::is_same_v<decltype((ty)), int &>);
-    assert((std::is_same_v<decltype(ty), int>));      // OK: one argument
-    assert((std::is_same_v<decltype((ty)), int &>));      // OK: one argument
+    int i = 67;
+    assert((std::is_same_v<decltype(i), int>));
+    assert((std::is_same_v<decltype((i)), int &>));
 
-    std::cout << std::is_same_v<decltype(ty), int> << std::endl;
-//    static_assert(std::is_same_v<decltype(ty), int &>);
+    Foo obj{};
+    assert((std::is_same_v<decltype(obj), Foo>));
+    assert((std::is_same_v<decltype((obj)), Foo &>));
 
     return 0;
 }
