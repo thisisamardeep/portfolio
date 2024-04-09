dd0e4b1c4492b38870191c638638daf47980e55c
amardeep <amardeepjk@gmail.com>
2024-04-09 20:29:38 +0800

    wip

--- khud.cpp
+++ khud.cpp
@@ -13,24 +13,35 @@ using namespace std;
 
 class Solution {
 public:
-    bool canJump(vector<int> &nums) {
-        bool res = true;
-        int reachable = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (reachable < i) {
-                res = false;
-                break;
-            } else {
-                reachable = max(reachable, i + nums[i]);
-            };
+    void myrec(vector<int> &nums, int currindex, int &countofways) {
+        if (currindex == nums.size() - 1) {
+            countofways++;
+            return;
         }
-        return res;
+        if (currindex > nums.size() - 1) {
+            return;
+        }
+        for (int i = 1; i <= nums[currindex]; i++) {
+            myrec(nums, currindex + i, countofways);
+        }
+    }
+
+public:
+    bool canJump(vector<int> &nums) {
+        int countofways = 0;
+        myrec(nums, 0, countofways);
+        if (countofways == 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 };
 
 int main() {
     Solution df{};
-    vector<int> nums{3, 2, 1, 0, 4};
+//    vector<int> nums{5, 2, 1, 0, 4};
+    vector<int> nums{2, 3, 1, 1, 4};
 //    vector<int> nums{2, 0, 0};
 
     bool res = df.canJump(nums);
