0bdbc71558a74a68a33d9edc6f1c69c423c50119
amardeep <amardeepjk@gmail.com>
2024-04-12 12:22:31 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -2,20 +2,9 @@
 //#include "iostream"
 #include "vector"
 
-class Complex {
-public:
-    int rt = 56;
-public:
-    Complex() = default;
-};
 
 
 int main() {
-    std::vector<int> gg{67, 777};
-//    int x1{1}, y1{2};
-    float x2{1.0}, y2{1.0};
-//    Complex x4{3, 5}, y3{3, 4};
-    Complex x4{6};
 
 
     return 0;
