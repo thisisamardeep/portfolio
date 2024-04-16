31fbdf1bc6c10ba55e9b179abc0eb7804b894516
amardeep <amardeepjk@gmail.com>
2024-04-17 00:14:19 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -1,17 +1,28 @@
 //#include "set"
 #include "iostream"
 
+using namespace std;
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+
+        // take first string as the lower string and second string as
+        // the upper string
+
+        // first we  run loop from back over the lower string
 
 
 
-
+    }
+};
 
 int main() {
     int ty = 67;
-    int t5=4555;
+    int t5 = 4555;
     int &yyy = ty;
 //    yyy = t5;
-    &yyy=&t5;
+//    &yyy = &t5;
 
     return 0;
 }
