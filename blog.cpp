4c1654421b89cdb548c92358461c35e4138985a0
amardeep <amardeepjk@gmail.com>
2024-05-03 06:36:56 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -8,50 +8,30 @@ using namespace std;
 
 class Solution {
 public:
-    int maximumPopulation(vector<vector<int>> &logs) {
-        vector<int> mypopulation(2051, 0);
-        int earlier_year = 0;
-        int max_population = 0;
-        for (int i = 0; i < logs.size(); i++) {
-            for (int j = logs[i][0]; j < logs[i][1]; j++) {
-                mypopulation[j] = mypopulation[j] + 1;
-                if (max_population < mypopulation[j]) {
-                    max_population = mypopulation[j];
-                    earlier_year = j;
-                } else if (max_population == mypopulation[j]) {
-                    if (earlier_year > j) {
-                        earlier_year = j;
-                    }
-                } else {
-
+    vector<int> dailyTemperatures(vector<int> &temperatures) {
+        vector<int> res(temperatures.size(), 0);
+        for (int i = 0; i < temperatures.size(); i++) {
+            int days = 0;
+            for (int j = i + 1; j < temperatures.size(); j++) {
+                days = days + 1;
+                if (temperatures[j] > temperatures[i]) {
+                    break;
+                } else if (j == temperatures.size() - 1) {
+                    days = 0;
                 }
-
             }
-
+            res[i] = days;
         }
-//        assert(2 == 2);
-        assert(mypopulation[2005] == 3);
-        assert(mypopulation[2040] == 3);
-
-        return earlier_year;
-
+        return res;
     }
 };
 
 int main() {
 
 
-    vector<vector<int>> logs{{2033, 2034},
-                             {2039, 2047},
-                             {1998, 2042},
-                             {2047, 2048},
-                             {2025, 2029},
-                             {2005, 2044},
-                             {1990, 1992},
-                             {1952, 1956},
-                             {1984, 2014}};
+    vector<int> temp{73, 74, 75, 71, 69, 72, 76, 73};
 
 
-    int rt = (new Solution)->maximumPopulation(logs);
+    vector<int> result = (new Solution)->dailyTemperatures(temp);
     return 0;
 }
--- rr.py
+++ rr.py
@@ -3,13 +3,29 @@ from typing import List
 
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
-        return 56
+        pop_array = [0] * 2051
+        max_year = 0
+        max_pop = 0
+        for i in range(0, len(logs), 1):
+            for j in range(logs[i][0], logs[i][1], 1):
+                pop_array[j] = pop_array[j] + 1
+                if max_pop < pop_array[logs[i][0]]:
+                    max_year = logs[i][0]
+                    max_pop = pop_array[logs[i][0]]
+                elif max_pop == pop_array[logs[i][0]]:
+                    if max_year > logs[i][0]:
+                        max_year = logs[i][0]
+                else:
+                    pass
+
+        return max_year
 
 
 er = Solution()
 e4 = [[1993, 1999], [2000, 2010]]
 eeee = er.maximumPopulation(e4)
 
+e222 = [4] * 3
 e222 = [4] * 3
 
 print(eeee)
