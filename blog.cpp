28a2d18245069a0951645bd7bae232727e47d8cc
amardeep <amardeepjk@gmail.com>
2024-05-05 11:12:49 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -3,34 +3,40 @@
 #include "iostream"
 #include "vector"
 #include "cassert"
-
+//https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
 using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
 public:
-    void myrec(int m, int n, int i, int j) {
-
-    }
-
-public:
-    int maximalSquare(vector<vector<char>> &matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        myrec(m, n, 0, 0);
-        int max_area = 0;
-        return max_area;
+    int minDepth(TreeNode *root) {
+        return 56;
     }
 };
 
 int main() {
 
-    vector<vector<char>> matrix1{{'1', '1', '1', '1', '0'},
-                                 {'1', '1', '1', '1', '0'},
-                                 {'1', '1', '1', '1', '1'},
-                                 {'1', '1', '1', '1', '1'},
-                                 {'0', '0', '1', '1', '1'}};
-
+    TreeNode t3 = TreeNode(3);
+    TreeNode t9 = TreeNode(9);
+    TreeNode t20 = TreeNode(20);
+    TreeNode t15 = TreeNode(15);
+    TreeNode t7 = TreeNode(7);
+    t3.left = &t9;
+    t3.right = &t20;
+    t20.left = &t15;
+    t20.left = &t7;
     Solution er{};
-    int res = er.maximalSquare(matrix1);
+    int res = er.minDepth(&t3);
     return 0;
 }
