e4011e06f9bf2d8b30317b8e9d7fcd6275163365
amardeep <amardeepjk@gmail.com>
2024-05-18 01:13:05 +0800

    wip

--- _posts/2023-03-09-decltype-quirks.md
+++ _posts/2023-03-09-decltype-quirks.md
@@ -81,7 +81,44 @@ int main() {
 }
 ```
 
+decltype can be very useful in perfect returning.
+```cpp
 
+
+const int &get_value_example1() {
+    int *t = new int(45);
+    return *t;
+}
+
+auto get_value_wrapped_example1() {
+    return get_value_example1();
+}
+
+const int &get_value_example2() {
+    int *t = new int(45);
+    return *t;
+}
+
+decltype(auto) get_value_wrapped_example2() {
+    return get_value_example2();
+}
+
+int main() {
+    // here due to normal type deduction rules first_example is deduced as int
+
+    decltype(auto) first_example = get_value_wrapped_example1();
+    static_assert(std::is_same<decltype(first_example), int>::value);
+
+//decltype(auto)   makes sure that type is perfectly returned from the 
+//wrapped function
+    decltype(auto) second_example = get_value_wrapped_example2();
+    static_assert(std::is_same<decltype(second_example), const int &>::value);
+
+    return 0;
+}
+
+
+```
 
 ```cpp
 
--- blog.cpp
+++ blog.cpp
@@ -9,20 +9,36 @@
 //write blog about how the dp works
 // do in via hashmap
 // do it in python
-const int &get_int_first_example() {
+
+
+const int &get_value_example1() {
     int *t = new int(45);
     return *t;
 }
 
-const int &get_int_second_example() {
+auto get_value_wrapped_example1() {
+    return get_value_example1();
+}
+
+const int &get_value_example2() {
     int *t = new int(45);
     return *t;
 }
 
-
+decltype(auto) get_value_wrapped_example2() {
+    return get_value_example2();
+}
 
 int main() {
+    // here due to normal type deduction rules first_example is deduced as int
 
+    decltype(auto) first_example = get_value_wrapped_example1();
+    static_assert(std::is_same<decltype(first_example), int>::value);
+
+//decltype(auto)   makes sure that type is perfectly returned from the
+//wrapped function
+    decltype(auto) second_example = get_value_wrapped_example2();
+    static_assert(std::is_same<decltype(second_example), const int &>::value);
 
     return 0;
 }
