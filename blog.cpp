37b123fb27048ff144c7a2fdc0a1d575d546ed44
amardeep <amardeepjk@gmail.com>
2024-05-25 03:04:21 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -10,16 +10,42 @@
 // do in via hashmap
 // do it in python
 
-
+#include "vector"
 #include "map"
+
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int> &nums) {
+        int curr_start = 0;
+        int curr_end = 0;
+        int jumps = 0;
+        while (true) {
+            if (curr_end == nums.size() - 1) {
+                break;
+            }
+            int new_end = 0;
+            int new_start = 0;
+            for (int i = curr_start; i <= curr_end; i++) {
+                new_end = max(new_end, i + nums[i]);
+            }
+            new_end = min(new_end, static_cast<int>(nums.size() - 1));
+            new_start = min(curr_end + 1, static_cast<int>(nums.size() - 1));
+            curr_start = new_start;
+            curr_end = new_end;
+            jumps = jumps + 1;
+        }
+        return jumps;
+    }
+};
 
 int main() {
 
-    std::map<int, std::string> m;
-    m[1] = "ee"; // first way
-    m.insert(std::make_pair(23, "ee"));// second way
-    m.insert(std::pair(34, "333")); // third way
-    m.insert({2, "err"});  //fourth way
-
+    vector<int> nums{2, 3, 1, 1, 4};
+    Solution *s = new Solution{};
+    int curr = 0;
+    vector<int> partial{};
+    int res = s->jump(nums);
     return 0;
 }
--- test.py
+++ test.py
@@ -1,42 +1,44 @@
 from typing import List
 
+from recviz import recviz
+
 
 class Solution:
     def __init__(self):
-        self.result = []
-
-    def amar(self, nums: List[int], curr_index: int, temp: List[int]):
-
-        # print(curr_index)
-        if curr_index >= len(nums):
-            return
-
-        # if curr_index == len(nums) - 1:
-        #     pass
-        temp.append(curr_index)
-        for i in range(1, nums[curr_index] + 1, 1):
-            temp_index = curr_index + i
-            self.amar(nums, temp_index, temp)
-        if curr_index == len(nums) - 1:
-            print(temp)
-            self.result.append(temp)
-        # print(temp)
-        temp.pop()
+        self.result = 9999999
+        self.jumps = 0
 
     def jump(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return 0
         if len(nums) == 2:
             return 1
-        temp = []
-        self.amar(nums, 0, temp)
-        return 333
+        jumps = 0
+        start_index = 0
+        end_index = 0
+        new_end_index = 0
+        new_start_index = 0
+
+
+
+        while True:
+            if new_end_index == len(nums) - 1:
+                break
+            new_start_index = min(end_index + 1, len(nums) - 1)
+            for i in range(start_index, end_index + 1, 1):
+                new_end_index = max(new_end_index, nums[i] + i)
+            new_end_index = min(new_end_index, len(nums) - 1)
+            jumps = jumps + 1
+            start_index = new_start_index
+            end_index = new_end_index
+
+        return jumps
 
 
 df = Solution()
-nums = [2, 3, 1, 1, 4]
+# nums = [2, 3, 1, 1, 4]
 # nums = [1, 2, 0, 1]
-# nums = [1, 2, 3]
+nums = [1, 2, 3, 1, 1, 1, 1, 1, 1, 1]
 res = df.jump(nums=nums)
 
 print(res)
