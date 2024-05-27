0269501eb17cad100dddd13b5820860d0f3ffdb1
amardeep <amardeepjk@gmail.com>
2024-05-27 13:51:09 +0800

    wip

--- rr.py
+++ rr.py
@@ -1,30 +1,4 @@
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-class Solution:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:  # root is none(null)
-            return 0
-        q = [[root, 1]]  # start with root and depth 1
-
-        mn = 10 ** 9
-        while len(q) > 0:
-            # x,d=q.pop(0)# for bfs
-            x, d = q.pop(-1)  # for dfs
-            c = 0
-            if x.left:
-                q.append([x.left, d + 1])
-                c += 1
-            if x.right:
-                q.append([x.right, d + 1])
-                c += 1
-            if c == 0:  # if left and right children are none .leaf nodes
-                mn = min(mn, d)  # at leaf compare distance from root.update minimum
-        return mn
+er = "er445"
+print(id(er))
+print(er.rstrip('4'))
+print(id(er[::-1]))
--- test.py
+++ test.py
@@ -4,11 +4,33 @@ from recviz import recviz
 
 
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
+    # @recviz
+    def myrec(self, n: int, left_used: int, right_used: int, partial_result: str, result: List[str],
+              ):
         pass
+        if left_used > n or right_used > n:
+            return
+        if left_used == n and right_used == n:
+            result.append(partial_result)
+            return
+
+        if right_used < left_used:
+            self.myrec(n, left_used + 1, right_used, partial_result + "(", result)
+            self.myrec(n, left_used, right_used + 1, partial_result + ")", result)
+        else:
+            pass
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        partial_result: str = "("
+        result: List[str] = []
+        curr_index = 0
+        if n == 0:
+            return []
+        self.myrec(n, 1, 0, partial_result, result)
+        return result
 
 
 df = Solution()
-err = df.generateParenthesis(4);
+err = df.generateParenthesis(3)
 
 print(err)
