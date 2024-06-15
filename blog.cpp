f90c658267c984febb60497aaf188497d1950986
amardeep <amardeepjk@gmail.com>
2024-06-15 12:39:51 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -5,9 +5,57 @@
 
 using namespace std;
 
+class Customer {
+private:
+    std::string name;
+    std::vector<int> values;
+public:
+    Customer(const std::string &n) : name{n} {
+
+    }
+
+
+    std::string getName() const {
+        return name;
+    }
+
+    void addValue(int val) {
+        values.push_back(val);
+    }
+
+    friend std::ostream &operator<<(std::ostream &strm, const Customer &cust) {
+        strm << ' [ ' << cust.name << ": ";
+        for (int val: cust.values) {
+            strm << val << ' ';
+        }
+        strm << ' ] ';
+        return strm;
+    }
+
+};
+
 
 int main() {
-    std::string er = "e34";
-    auto ee = er[0];
+    Customer c{"ttt ttt yutrrf erwwv wf23f e3tdx"};
+    for (int val: std::initializer_list<int>{0, 8, 15}) {
+        std::cout << val << std::endl;
+        c.addValue(val);
+    }
+    std::cout << std::addressof(c) << std::endl;
+
+    std::vector<Customer> customers;
+//    customers.push_back(c);
+        customers.push_back(std::move(c)); // move into the vector
+
+    std::cout << c.getName() << std::endl;
+    std::cout << std::addressof(customers[0]) << std::endl;
+
+//    std::cout << std::addressof(customers[0].) << std::endl;
+
+//    customers.push_back(std::move(c)); // move into the vector
+//    std::cout << "customers:\n";
+//    for (const Customer &cust: customers) {
+//        std::cout << " " << std::addressof(cust) << ' \n ';
+//    }
     return 0;
 };
--- rr.py
+++ rr.py
@@ -2,36 +2,28 @@ from typing import List
 
 
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        temp_sum = 0
-        max_difference = 200000
-        final_sum = 0
-        nums.sort()
-        for i in range(0, len(nums) - 2, 1):
-            first_pointer = i + 1
-            second_pointer = len(nums) - 1
-            while first_pointer < second_pointer:
-                temp_sum = nums[i] + nums[first_pointer] + nums[second_pointer]
-                if temp_sum >= target:
-                    difference = temp_sum - target
-                else:
-                    difference = target - temp_sum
-                if difference <= max_difference:
-                    max_difference = difference
-                    final_sum = temp_sum
-                if difference == 0:
-                    return final_sum
-                if temp_sum >= target:
-                    second_pointer = second_pointer - 1
-                else:
-                    first_pointer = first_pointer + 1
-        return final_sum
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        if len(sequence) == 1 and len(word) == 1 and word == sequence:
+            return 1
+        outer_counter = 0
+        k = 0
+        while outer_counter < len(sequence) - len(word) - 1:
+            inner_counter = outer_counter
+            while inner_counter < len(sequence) - 1:
+                if (
+                        sequence[
+                        inner_counter: min(
+                            inner_counter + len(word), len(sequence) - 1
+                        ): 1
+                        ]
+                        == word
+                ):
+                    k = k + 1
+                inner_counter = inner_counter + len(word)
+            outer_counter = outer_counter + 1
+        return k
 
-# 0 3 5
-er = [-5, -5, -4, 0, 0, 3, 3, 4, 5]
-# print(er.sort())
-# print(er)
-s = Solution()
 
-res = s.threeSumClosest(er, -2)
+sd = Solution()
+res = sd.maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba")
 print(res)
