7355a02a1a07a6a00d9ac2ff47a5db8e3fb590ff
amardeep <amardeepjk@gmail.com>
2024-07-16 18:09:40 +0800

    wip

--- test.py
+++ test.py
@@ -1,52 +1,8 @@
-from typing import List
-
-"""
-We take 2 index low and high.
-Low is the starting index and high is the max index.
-Now we dont know how may bianry slipts we need to do so 
-we rul a loop till low < hoigh
-
-
-"""
-
-
 class Solution:
-    def singleNonDuplicate(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        low = 0
-        high = len(nums) - 1
-        mid = 0
+    def mySqrt(self, x: int) -> int:
+        pass
 
-        while low < high:
-
-            mid = (low + high) // 2
-
-            if mid == low and low == 0:
-                return nums[low]
-            if mid == low and high == 0:
-                return nums[high]
-
-            if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
-                return mid
-
-            if mid % 2 == 0:
-                if nums[mid] == nums[mid + 1]:
-                    low = mid
-                elif nums[mid] == nums[mid - 1]:
-                    high = mid
-            elif mid % 2 == 1:
-                if nums[mid] == nums[mid + 1]:
-                    high = mid
-                elif nums[mid] == nums[mid - 1]:
-                    low = mid
-
-        return mid
-
-
-nums = [1, 2, 2, 3, 3]
 
 sd = Solution()
 
-res = sd.singleNonDuplicate(nums=nums)
-print(res)
+res = sd.mySqrt(45)
