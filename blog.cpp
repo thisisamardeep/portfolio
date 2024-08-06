d1fd5f6021b3c3cf6b4d8a005b39a8df1e1a4243
amardeep <amardeepjk@gmail.com>
2024-08-06 20:34:49 +0800

    wip

--- blog.cpp
+++ blog.cpp
@@ -2,34 +2,94 @@
 #include "iostream"
 #include "thread"
 #include "vector"
+#include "queue"
 
-class Base {
-public:
-    virtual void someFunction() {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 
-    }
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+                                                       right(right) {}
 };
 
-class Derived : public Base {
+class Solution {
 public:
-    using Base::someFunction;
-    virtual void someFunction(int i){
+    bool findTarget(TreeNode *root, int k) {
 
+        std::vector<int> res{};
+
+        std::queue<TreeNode *> myqueue{};
+        myqueue.push(root);
+        TreeNode *frontelement{};
+        int length_of_queue = myqueue.size();
+
+        while (myqueue.size() != 0) {
+            length_of_queue = myqueue.size();
+            for (int i = 0; i < length_of_queue; i++) {
+                frontelement = myqueue.front();
+                res.push_back(frontelement->val);
+                if (frontelement->left != nullptr) {
+                    myqueue.push(frontelement->left);
+                }
+                if (frontelement->right != nullptr) {
+                    myqueue.push(frontelement->right);
+                }
+                myqueue.pop();
+            }
+        }
+
+        std::sort(res.begin(), res.end());
+        int fpointer = 0;
+        int spointer = res.size()-1;
+        while (fpointer < spointer) {
+            if (res[fpointer] + res[spointer] == k) {
+                return true;
+            }
+            if (res[fpointer] + res[spointer] < k) {
+                fpointer++;
+            } else {
+                spointer--;
+            }
+        }
+
+        return false;
     }
-
 };
 
 int main() {
+    TreeNode t5{5};
+    TreeNode t3{3};
+    TreeNode t2{2};
+    TreeNode t4{4};
+    TreeNode t6{6};
+    TreeNode t7{7};
 
+    t5.left = &t3;
+    t5.right = &t6;
+    t3.left = &t2;
+    t3.right = &t4;
+    t6.right = &t7;
 
-    std::cout << 56 << std::endl;
+    Solution dfr{};
+    auto rtttt = dfr.findTarget(&t5,9);
+
     return 0;
-};
-/***
-*
- *
- *
- *
- *
- *
-*/
+}
--- er4.py
+++ er4.py
@@ -1,5 +1,9 @@
-sd = [3, 4, 5, 6, ]
-
-sd.insert(2, 44)
-
-print(sd)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        pass
