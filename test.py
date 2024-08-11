3b7fd4f863957cdeb655f3760c73d63b25c4a113
amardeep <amardeepjk@gmail.com>
2024-08-11 22:17:29 +0800

    wip

--- test.py
+++ test.py
@@ -1,25 +1,3 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+rt = [5, 67, 677]
 
-
-from typing import Optional, List
-
-
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        stack = []
-        curr = root
-
-        while curr is not None or len(stack) != 0:
-            while curr is not None:
-                stack.append(curr)
-                curr = curr.left
-            curr = stack.pop()
-            res.append(curr.val)
-            curr = curr.right
-        return  res
+print(max(rt))
